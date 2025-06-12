97f15ec8cf2a3b7857c3b837e2e14cd1e9785ab3
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-06-12 13:18:45 +0200

    Change file name

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,38 @@ import GeneralInfoForm from "./components/GeneralInfoForm";
 import EducationForm from "./components/EducationForm";
 import SkillsForm from "./components/SkillsForm";
 import ExperienceForm from "./components/ExperienceForm";
-import ProjectsForm from "./components/Projects";
+import ProjectsForm from "./components/ProjectsForm";
 import Form from "./components/Form";
+import ResumePreview from "./components"
 
 function App() {
   return (
     <>
-      <Form>
-        <h2>General Information</h2>
-        <GeneralInfoForm />
-      </Form>
-      <Form>
-        <h2>Education</h2>
-        <EducationForm />
-      </Form>
-      <Form>
-        <h2>Skills</h2>
-        <SkillsForm />
-      </Form>
-      <Form>
-        <h2>Experience</h2>
-        <ExperienceForm />
-      </Form>
-      <Form>
-        <h2>Projects</h2>
-        <ProjectsForm />
-      </Form>
+      <div>
+        <Form>
+          <h2>General Information</h2>
+          <GeneralInfoForm />
+        </Form>
+        <Form>
+          <h2>Education</h2>
+          <EducationForm />
+        </Form>
+        <Form>
+          <h2>Skills</h2>
+          <SkillsForm />
+        </Form>
+        <Form>
+          <h2>Experience</h2>
+          <ExperienceForm />
+        </Form>
+        <Form>
+          <h2>Projects</h2>
+          <ProjectsForm />
+        </Form>
+      </div>
+      <div>
+
+      </div>
     </>
   );
 }
