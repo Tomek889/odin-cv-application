f82535182a859dade2b5dd8829e4a33d7c26ad1a
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-06-12 12:18:34 +0200

    Change body background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import "./styles/reset.css"
+import "./styles/reset.css";
+import "./styles/share.css";
 import "./styles/formStyles.css";
-import "./styles/resume.css"
+import "./styles/resume.css";
 
 import GeneralInfoForm from "./components/GeneralInfoForm";
 import EducationForm from "./components/EducationForm";
@@ -12,26 +13,26 @@ import Form from "./components/Form";
 function App() {
   return (
     <>
-    <Form>
-      <h2>General Information</h2>
-      <GeneralInfoForm />
-    </Form>
-    <Form>
-      <h2>Education</h2>
-      <EducationForm />
-    </Form>
-    <Form>
-      <h2>Skills</h2>
-      <SkillsForm />
-    </Form>
-    <Form>
-      <h2>Experience</h2>
-      <ExperienceForm />
-    </Form>
-    <Form>
-      <h2>Projects</h2>
-      <ProjectsForm />
-    </Form>
+      <Form>
+        <h2>General Information</h2>
+        <GeneralInfoForm />
+      </Form>
+      <Form>
+        <h2>Education</h2>
+        <EducationForm />
+      </Form>
+      <Form>
+        <h2>Skills</h2>
+        <SkillsForm />
+      </Form>
+      <Form>
+        <h2>Experience</h2>
+        <ExperienceForm />
+      </Form>
+      <Form>
+        <h2>Projects</h2>
+        <ProjectsForm />
+      </Form>
     </>
   );
 }
